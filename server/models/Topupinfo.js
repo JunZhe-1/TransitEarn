49a9052e4a0ae61e50746137ba0e7f66b566295a
Jun Zhe <136241274+JunZhe-1@users.noreply.github.com>
2023-06-14 12:10:39 +0800

    Update Topupinfo.js

--- server/models/Topupinfo.js
+++ server/models/Topupinfo.js
@@ -2,12 +2,33 @@ module.exports = (sequelize, DataTypes) => {
     const Topupinfo = sequelize.define("Topupinfo", {
     cardNo: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        primaryKey: true,
+
     },
     balance: {
     type: DataTypes.DOUBLE,
     allowNull: false
-    }
-    });
+    },
+    cvv:{
+        type: DataTypes.STRING,
+        allowNull : false
+    },
+
+    },
+
+{primaryKey: 'cardNo'}
+
+    );
+
+
+
+    Topupinfo.associate = (models) => {
+        Topupinfo.hasMany(models.Ezlink, {
+            foreignKey: "cardNo",
+            onDelete: "cascade"
+        });
+    };
+
     return Topupinfo;
     }
