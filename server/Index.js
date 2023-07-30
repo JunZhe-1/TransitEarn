9e23519970df7c5a823b79593254987dcefae373
JunZhe-1 <2931801324qq@gmail.com>
2023-07-30 19:05:46 +0800

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -9,11 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-<<<<<<< Updated upstream
-    res.send("This is server");
-=======
-    res.send("This is bank");
->>>>>>> Stashed changes
 });
 
 const topupRoute = require("./routes/topupinfo")
